cd670625a407a079d7600b9cdc3105db317acddc
evgenykatyshev <me@ekatyshev.ru>
2020-11-01 22:53:33 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+
+<html lang="en" prefix="og: http://ogp.me/ns#">
+
+<head>
+
+<base href="//twister-roulette.com/" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="index, follow" />
+<meta name="viewport" content="width=device-width" />
+
+<link rel='manifest' href='/manifest.json' /><!--
+<script type="module">
+/*
+ This code uses the pwa-update web component https://github.com/pwa-builder/pwa-update to register your service worker,
+ tell the user when there is an update available and let the user know when your PWA is ready to use offline.
+*/
+
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
+</script>-->
+
+<!-- jQuery -->
+<script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="js/jquery.cookie.js"></script>
+
+<!-- Языки -->
+<script type="text/javascript" src="js/i18n.js"></script>
+
+<!-- Лайкли -->
+<script src="js/likely-0.91/likely.js"></script>
+<link rel="stylesheet" href="js/likely-0.91/likely.css">
+
+<!-- Рулетка -->
+<script type="text/javascript" src="js/roulette.js"></script>
+
+<title>Roulette for Twister</title>
+
+<meta name="description" content="Online roulette for playing Twister" />
+<meta name="keywords" content="roulette,twister,online" />
+
+<meta property="og:image" content="//twister-roulette.com/img/twister-roulette-ru.png" />
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,500&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet" />
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="icon" type="image/png" href="favicon.png" />
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter26947902 = new Ya.Metrika({id:26947902,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<!-- /Yandex.Metrika counter -->
+
+</head>
+
+<body>
+
+<?php include_once('img/icons.svg'); ?>
+
+<header id="header">
+	<h1 id="title">Roulette for Twister</h1><!--
+  <div class="button button_settings">
+	  <svg class="button__icon">
+			<use xlink:href="#settings_icon"></use>
+		</svg>
+	</div>--><!--
+	<div class="button button_sound">
+	  <svg class="button__icon button__icon_off">
+			<use xlink:href="#sound_off_icon"></use>
+		</svg>
+		<svg class="button__icon button__icon_on">
+			<use xlink:href="#sound_on_icon"></use>
+		</svg>
+	</div>-->
+  <div class="button button_lang">en</div>
+  <ul id="lang-list">
+    <li><a rel="alternate" hreflang="be" href="be" class="lang">Белорусский</a></li>
+    <li><a rel="alternate" hreflang="de" href="de" class="lang">Deutsch</a></li>
+    <li><a rel="alternate" hreflang="en" href="en" class="lang">English</a></li>
+    <li><a rel="alternate" hreflang="es" href="es" class="lang">Español</a></li>
+    <li><a rel="alternate" hreflang="fr" href="fr" class="lang">Français</a></li>
+    <li><a rel="alternate" hreflang="it" href="it" class="lang">Italiano</a></li>
+    <li><a rel="alternate" hreflang="ru" href="ru" class="lang">Русский</a></li>
+    <li><a rel="alternate" hreflang="uk" href="uk" class="lang">Українська</a></li>
+    <li><a rel="alternate" hreflang="zh" href="zh" class="lang">中文</a></li>
+  </ul>
+	<div id="timer" class="hidden">
+	  <div class="button button_minus enable">
+			<svg class="button__icon">
+				<use xlink:href="#minus_icon"></use>
+			</svg>
+  	</div>
+		<div id="time_period"></div>
+		<div id="time"></div>
+	  <div class="button button_plus enable">
+			<svg class="button__icon plus_icon">
+				<use xlink:href="#plus_icon"></use>
+			</svg>
+	  </div>
+	</div>
+	<div class="button button_play">
+		<svg class="button__icon">
+			<use xlink:href="#timer_off_icon"></use>
+		</svg>
+	</div>
+	<div class="button button_stop">
+		<svg class="button__icon">
+			<use xlink:href="#timer_icon"></use>
+		</svg>
+	</div>
+	<div id="tip" class="desktop-part">Click anywhere or press any key</div>
+  <div id="about">
+    <span id="author"><span class="desktop-part">Design: </span><a href="//evgenykatyshev.ru/projects/twister-roulette/" class="link"><span class="desktop-part">Evgeny </span>Katyshev</a></span>
+    <a id="old" href="//old.twister-roulette.com/" class="link desktop-part">Old version</a>
+  </div>
+</header>
+
+<div id="settings" class="hidden">
+  <div class="button button_close">
+	  <svg class="button__icon">
+			<use xlink:href="#close_icon"></use>
+		</svg>
+	</div>
+</div>
+
+<div id="main">
+	<div id="limb"></div>
+	<div id="circle"></div>
+	<div id="color-name"></div>
+</div>
+
+</body>
+</html>
---
+++ js/roulette.js
@@ -0,0 +1,246 @@
+$(document).ready(function() {
+
+  var lang;
+
+  var setLang = function() {
+    var pathname = window.location.pathname;
+    var pathnameArr = pathname.split('/');
+    urlLang = pathnameArr[1];
+    //alert('from URL: '+lang);
+    if(urlLang != 'be' && urlLang != 'de' && urlLang != 'en' && urlLang != 'es' && urlLang != 'fr' && urlLang != 'it' && urlLang != 'ru' && urlLang != 'uk' && urlLang != 'zh') {
+      cookieLang = $.cookie('lang'); // Get language from cookies
+      if(cookieLang != 'be' && cookieLang != 'de' && cookieLang != 'en' && cookieLang != 'es' && cookieLang != 'fr' && cookieLang != 'it' && cookieLang != 'ru' && cookieLang != 'uk' && cookieLang != 'zh')
+        lang = 'en'; // Default language
+      else {
+        lang = cookieLang;
+        $.cookie('lang', lang, { path: '/' });
+      }
+    }
+    else {
+      lang = urlLang;
+      $.cookie('lang', lang, { path: '/' });
+    }
+    //alert('final lang: '+lang);
+  }
+
+  var colors = {
+    'libm': ['210', '220', '120', '012'],
+    'circle': ['e20', 'fc0', '6c3', '26c']
+  };
+
+  setLang();
+
+  //alert(lang);
+
+  $('.button_lang').text(lang);
+
+  var title = strings['title'][lang];
+  var tip = strings['tip'][lang];
+
+  $('title').html(title);
+  $('#title').html(title);
+  $('#tip').text(tip);
+  $('#old').attr('href', '//old.twister-roulette.com/'+lang);
+
+  var colorNames = [
+    strings['red'][lang],
+    strings['yellow'][lang],
+    strings['green'][lang],
+    strings['blue'][lang]
+  ];
+
+  var limbsNames = [
+    strings['rightHand'][lang],
+    strings['leftHand'][lang],
+    strings['rightFoot'][lang],
+    strings['leftFoot'][lang]
+  ];
+
+  var limbDiv = $('#limb');
+  var circleDiv = $('#circle');
+  var colorNameDiv = $('#color-name');
+  var favicon = $('link[rel="icon"]');
+  var lang = $('#lang span');
+
+  var timerInterval;
+  var time = 20;
+  var time_period = 20;
+
+  var time_decrement;
+  var time_increment;
+
+  function timer() {
+    if(time == 1) {
+      time = time_period;
+      spin();
+    }
+    else {
+      time--;
+      $('#time').html(time);
+    }
+  }
+
+  var spin = function() {
+    var color = Math.ceil(4 * Math.random()) - 1;
+    var limb = Math.ceil(4 * Math.random()) - 1;
+    limbDiv.html(limbsNames[limb]);
+    circleDiv.css('background', '#'+colors['circle'][color]);
+    lang.css('background', '#'+colors['circle'][color]);
+    colorNameDiv.html(colorNames[color]);
+    favicon.attr('href', '../img/favicon-'+color+'.png');
+    time = time_period;
+    $('#time').html(time_period);
+    return false;	
+  }
+
+  var adaptation = function() {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    
+    var likely = $('.likely');
+    if(windowWidth <= 600)
+      likely.addClass('likely-small');
+    else
+      likely.removeClass('likely-small');
+  }
+
+  spin();	
+  adaptation();
+
+  $('#main').on('click tap taphold', function() {
+    spin();
+    $('#title').hide();
+    $('#tip').hide();
+    $('#lang-list').hide();
+  });
+  $(document).keyup(spin);
+
+  $(window).on('load resize', adaptation);
+
+  // Buttons
+
+  $('.button_settings').on('click tap taphold', function() {
+    $('#settings').removeClass('hidden');
+    return false;
+  });
+
+  $('.button_close').on('click tap taphold', function() {
+    $('#settings').addClass('hidden');
+    return false;
+  });
+
+  $('.button_sound').on('click tap taphold', function() {
+    $('.button_sound > .button__icon_on').toggle();
+    $('.button_sound > .button__icon_off').toggle();
+    return false;
+  });
+
+  $('.button_play').on('click tap taphold', function() {
+    $('#title').hide();
+    $('#tip').hide();
+    $('.button_play').hide();
+    $('.button_stop').show();
+    $('#timer').removeClass('hidden');
+    spin();
+    timerInterval = setInterval(timer, 1000);
+    return false;
+  });
+
+  $('.button_stop').on('click tap taphold', function() {
+    $('.button_stop').hide();
+    $('.button_play').show();
+    $('#timer').addClass('hidden');
+    clearInterval(timerInterval);
+    return false;
+  });
+
+  $('.button_minus.enable').on('click tap taphold', function() {
+    $('#time_period').show();
+    switch(time_period) {
+      case 5:
+        time_decrement = 0;
+        time_period = 5;
+        break;
+      case 10:
+        time_decrement = 5;
+        time_period = 5;
+        $('.button_minus').removeClass('enable');
+        $('.button_minus').addClass('disable');			
+        break;
+      case 15:
+        time_decrement = 5;
+        time_period = 10;
+        break;
+      case 20:
+        time_decrement = 5;
+        time_period = 15;
+        break;
+      case 30:
+        time_decrement = 10;
+        time_period = 20;
+        break;
+      case 45:
+        time_decrement = 15;
+        time_period = 30;
+        $('.button_plus').removeClass('disable');
+        $('.button_plus').addClass('enable');
+        break;
+    }
+    if(time - time_decrement < 1)
+      spin();
+    else
+      time = time - time_decrement;
+    $('#time').html(time);
+    $('#time_period').html(time_period);
+    $('#time_period').delay(500).hide('slow');
+    return false;
+  });
+
+  $('.button_plus.enable').on('click tap taphold', function() {
+    $('#time_period').show();
+    switch(time_period) {
+      case 5:
+        time_increment = 5;
+        time_period = 10;
+        $('.button_minus').removeClass('disable');
+        $('.button_minus').addClass('enable');
+        break;
+      case 10:
+        time_increment = 5;
+        time_period = 15;				
+        break;
+      case 15:
+        time_increment = 5;
+        time_period = 20;
+        break;
+      case 20:
+        time_increment = 10;
+        time_period = 30;
+        break;
+      case 30:
+        time_increment = 15;
+        time_period = 45;
+        $('.button_plus').removeClass('enable');
+        $('.button_plus').addClass('disable');
+        break;
+      case 45:
+        time_increment = 0;
+        time_period = 45;
+        break;
+    }
+    time = time + time_increment;
+    $('#time_period').html(time_period);
+    $('#time').html(time);
+    $('#time_period').delay(500).hide('slow');
+    return false;
+  });
+
+  $('.button_lang').on('click tap taphold', function() {
+    $('#lang-list').show();
+  });
+
+  /*$('.lang').on('click tap taphold', function() {
+    $('#lang-list').hide();
+  });*/
+
+});	
